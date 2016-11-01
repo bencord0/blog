30d48aed772d92d38a9e4fbd876dfeb7d8516a9c
Ben Cordero <bencord0@condi.me>
2016-11-01 20:15:55 +0000

    Fixed upstream docker base image

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM bencord0/python:3.5.2
 
+ENV LD_LIBRARY_PATH=/usr/lib64/postgresql-9.5/lib64
 EXPOSE 8000
 ENTRYPOINT ["/usr/bin/blog"]
 CMD ["-b", "0.0.0.0:8000"]
 
-ADD ./dist/etc /etc
-ADD ./dist/usr/lib64 /usr/lib64
 ADD ./blog.pex /usr/bin/blog
+
+RUN /usr/bin/blog manage collectstatic --noinput
--- build.sh
+++ build.sh
@@ -10,11 +10,5 @@ source venv/bin/activate
 pip install --upgrade -r requirements.txt
 pex -vv -o blog.pex --no-wheel --disable-cache . -m blog
 
-mkdir -p dist/etc dist/usr/lib64
-echo 'hosts: files dns' > dist/etc/nsswitch.conf
-cp -a /usr/lib64/postgresql/libpq.so* dist/usr/lib64/
-cp -a /usr/lib/libldap-2.4.so.2* dist/usr/lib64/
-cp -a /usr/lib64/liblber-2.4.so.2* dist/usr/lib64
-
 docker build -t bencord0/blog .
 docker save bencord0/blog > docker-blog.tar
