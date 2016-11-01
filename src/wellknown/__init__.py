b68dfa1fb88b8175c4884003c4166314db64323b
Ben Cordero <bencord0@condi.me>
2016-11-01 20:26:46 +0000

    wellknown-keybase: Send 404 if proof is not present

--- src/wellknown/__init__.py
+++ src/wellknown/__init__.py
@@ -3,14 +3,18 @@ from django.http import HttpResponse
 
 
 def keybase(request):
-    if settings.WELLKNOWN_KEYBASE:
-        with open(settings.WELLKNOWN_KEYBASE, 'rb') as f:
-            content = f.read()
-            code = 200
-    else:
-        content = b''
-        code = 404
+    content = b''
+    code = 404
 
-    return HttpResponse(content,
-                        content_type='text/plain' if content else None,
-                        status=code)
+    if settings.WELLKNOWN_KEYBASE:
+        try:
+            with open(settings.WELLKNOWN_KEYBASE, 'rb') as f:
+                content = f.read()
+                code = 200
+        except FileNotFoundError:
+            pass
+
+    return HttpResponse(
+        content,
+        content_type='text/plain' if content else None,
+        status=code)
