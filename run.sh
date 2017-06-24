b44410eaf7eadfbafd9a323114f09f3261e72d84
Ben Cordero <bencord0@condi.me>
2017-06-24 23:40:50 +0100

    run.sh: wait for db to startup

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker-compose up -d
 docker-compose exec blog bash -c \
-  'git clone https://github.com/bencord0/blogposts; \
+  'git clone https://github.com/bencord0/blogposts; sleep 5; \
    blog manage migrate && blog manage import_entries /blogposts'
 docker-compose logs -f
 docker-compose down
