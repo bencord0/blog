384ea7436e47ec0ecc59040d480ac5b2f766e54a
Ben Cordero <bencord0@condi.me>
2017-09-20 13:44:53 +0100

    Implement get_recent_entries by glob

--- src/blog/utils.py
+++ src/blog/utils.py
@@ -1,3 +1,5 @@
+from glob import glob
+
 from jinja2 import Environment, FileSystemLoader
 
 from markdown import Markdown
@@ -13,7 +15,23 @@ def static(path):
 
 
 def get_recent_entries(count=10):
-    return []
+    meta = glob("metadata/*.json")
+    entries = []
+
+    for m in meta:
+        with open(m, 'rb') as f:
+            entry = json.load(f)
+        entries.append(entry)
+
+    return entries
+    return [
+        {
+            "slug": entry["slug"],
+            "title": entry["title"],
+            "fuzzy_date": entry["date"],
+            "summary": entry["slug"],
+        } for entry in entries[:count]
+    ]
 
 
 def render_template(template, **context):
