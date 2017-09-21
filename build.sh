dfc65a3857c427abd6817f0e53aa0046857382fe
Ben Cordero <bencord0@condi.me>
2017-09-21 13:21:01 +0100

    remove custom build scripts

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xe
-
-docker build -t bencord0/blog .
-docker save -o docker.tar bencord0/blog
--- run.sh
+++
@@ -1,6 +0,0 @@
-docker-compose up -d
-docker-compose exec blog bash -c \
-  'git clone https://github.com/bencord0/blogposts; sleep 5; \
-   blog manage migrate && blog manage import_entries /blogposts'
-docker-compose logs -f
-docker-compose down
