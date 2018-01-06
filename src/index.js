14d4ea45bf7bfe449b2a16638089dec3818e3850
Ben Cordero <bencord0@condi.me>
2018-01-06 14:52:01 +0000

    Add bootstrap and a router

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
