46d6aee0200955a9886116c17b2263e8b682f0f8
Ben Cordero <bencord0@condi.me>
2026-08-04 15:31:30 +0100

    Update dependencies

--- manage.py
+++ manage.py
@@ -20,9 +20,6 @@
 import os
 import sys
 
-from psycopg2cffi import compat
-compat.register()
-
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     from django.core.management import execute_from_command_line
